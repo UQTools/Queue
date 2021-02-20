e6d2a0f047d8b31701067e3cbc1134c918f7c028
Mike <mikepham1207@gmail.com>
2021-02-20 16:28:46 +1000

    Eslint and prettier

--- src/server/types/env.d.ts
+++ src/server/types/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
-  export interface ProcessEnv {
-    DB_URL: string;
-    NODE_ENV: string;
-  }
+    export interface ProcessEnv {
+        DB_URL: string;
+        NODE_ENV: string;
+    }
 }
