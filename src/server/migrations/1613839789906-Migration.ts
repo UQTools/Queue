8e14bbfe7cb1d1cc17b4301580386f34d1bebd71
Mike <mikepham1207@gmail.com>
2021-02-21 03:17:38 +1000

    Forgot a migration again

---
+++ src/server/migrations/1613839789906-Migration.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1613839789906 implements MigrationInterface {
+    name = "Migration1613839789906";
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "queue" ADD "shortDescription" character varying NOT NULL`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "queue" DROP COLUMN "shortDescription"`
+        );
+    }
+}
