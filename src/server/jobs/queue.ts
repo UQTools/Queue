86991d20a09abf5a81b96b1b0608991df4a87495
Mike <mikepham1207@gmail.com>
2021-05-21 16:30:50 +1000

    Change question statuses to closed without removing them at midnight

--- src/server/jobs/queue.ts
+++ src/server/jobs/queue.ts
@@ -1,19 +1,25 @@
 import { RecurrenceRule } from "node-schedule";
 import { In } from "typeorm";
 import { CourseUserMeta, Question, Queue } from "../entities";
+import { QuestionStatus } from "../types/question";
 
 export const endOfDayRule = new RecurrenceRule();
-endOfDayRule.hour = 0;
+endOfDayRule.hour = 23;
+endOfDayRule.minute = 59;
 endOfDayRule.tz = "Australia/Brisbane";
 
 export const resetQueues = async () => {
     const queues = await Queue.find({
         clearAfterMidnight: true,
     });
-    const questionsToDelete = await Question.find({
+    const questionsToClose = await Question.find({
         queueId: In(queues.map((queue) => queue.id)),
     });
-    await Question.remove(questionsToDelete);
+    for (const question of questionsToClose) {
+        question.closedTime = new Date();
+        question.status = QuestionStatus.CLOSED;
+    }
+    await Question.save(questionsToClose);
 };
 
 export const resetQuestionCount = async () => {
